ad8cf97aad580bf0eb7d07faa78e9832339253f4
rohitprajapati9 <99532837+rohitprajapati9@users.noreply.github.com>
2022-05-03 21:58:21 +0545

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["front-end developer","Enthusiastic programmer","cross-platform developer","react developer"],
+        strings: ["front-end developer","Enthusiastic programmer","cross-platform developer","Angular developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["front-end developer","Enthusiastic programmer","cross-platform developer","react developer"],
+        strings: ["front-end developer","Enthusiastic programmer","cross-platform developer","Angular developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
